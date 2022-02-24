3bd29ddd77e8f6186767d745ed0a59ead88f3226
ananya karne <ananyakarne02@gmail.com>
2022-02-24 20:54:36 +0530

    made a circle class.

--- 23feb22/rectangle.cpp
+++ 23feb22/rectangle.cpp
@@ -3,23 +3,53 @@ using namespace std;
 
 class rectangle
 {
-    public:
+public:
     int l;
     int b;
-    
-    int area(){
-        return l*b;
+
+    int area()
+    {
+        return l * b;
     }
-     int perimeter(){
-         return 2*(l+b);
-     }
+    int perimeter()
+    {
+        return 2 * (l + b);
+    }
 };
+
+class circle
+{
+public:
+    int r;
+    float pi = 3.14;
+
+    float area()
+    {
+
+        return pi * r * r;
+    }
+    float circumference()
+    {
+        return 2*pi*r;
+    }
+    float getdiameter()
+    {
+        return r*2;
+    }
+};
+
+
 int main()
 {
     rectangle rec;
-    rec.l=333320;
-    rec.b=50;
-    cout<<rec.area()<<endl;
-    cout<<rec.perimeter()<<endl;
+    rec.l = 333320;
+    rec.b = 50;
+    cout << rec.area() << endl;
+    cout << rec.perimeter() << endl;
 
+    circle cir;
+    cir.r=1;
+    cout << cir.area() << endl;
+    cout << cir.circumference() << endl;
+    cout << cir.getdiameter() << endl;
 }

ffd7d42c64e58f16ef75ee7be3c645d8ebc8c203
ananya karne <ananyakarne02@gmail.com>
2022-04-01 23:43:17 +0530

    continue

---
+++ 1apr22/trials.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i =0;i<5;i++){
+        
+        if (i==2){
+        continue;
+        }
+         cout<<"hey"<<i<<endl;
+        
+        
+    }
+//     for (int i = 0; i < 5; i++) {
+//   if (i == 4) {
+//     continue;
+//   }
+//   cout << "hey\n";
+// }
+
+}

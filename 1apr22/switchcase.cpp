fa9188135861f5d2ce8bfb0dccca82c5414a5875
ananya karne <ananyakarne02@gmail.com>
2022-04-01 23:25:43 +0530

    net switch

--- 1apr22/switchcase.cpp
+++ 1apr22/switchcase.cpp
@@ -3,14 +3,21 @@ using namespace std;
 int main()
 {
     char a = 'r';
-    int num = 2;
-    switch (num)
+    int num = 1;
+    switch (a)
     {
     case 1:
         cout << "no1";
         break;
+        
     case 'r':
-        cout << "nope2";
+        // cout << "nope2";
+        switch(a){
+            case 'r':
+            cout<<"2nd switch"<<endl;;
+            break;
+        }
+        cout<<"character out"<<endl;
         break;
     case 2:
         cout << "yes 3";

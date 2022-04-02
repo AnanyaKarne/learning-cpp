f8867a28a338aacb74f2bac18c99601d2a3dc054
ananya karne <ananyakarne02@gmail.com>
2022-04-03 00:16:43 +0530

    finding number of number needed to make a given number using switch statement

--- 2apr22/finding_number.cpp
+++ 2apr22/finding_number.cpp
@@ -1,77 +1,53 @@
 #include <iostream>
 using namespace std;
-// int temp=0;
-// int a=1330;
-
-// void hundred(){
-//     int a;
-//     temp=a/100;
-// }
-// void fifty(){
-//     int a;
-//     temp=a/50;
-// }
-// void twenty(){
-//     int a;
-//     temp=a/20;
-// }
-// void one(){
-//     int a;
-//     temp=a/1;
-// }
-// int main(){
-//     int a;
-//     if(a>100){
-//         hundred();
-//     }
-
-// int a, h;
-// int q=0;
-// int w=0;
-// a=1300;
-// h=100;
-// q=a/h;
-// cout<<"number of 100's note needed are: "<<q;
-
-// if(a>0){
-//     a/
-//}
-// }
 
 int main()
 {
-    int num_of_hundreds;
-    int num_of_fiftys;
-    int num_of_twentys;
-    int num_of_ones;
-    int num ;
-    cout<<"enter a number"<<endl;
-    cin>>num;
-    int a = 0;
-    if (num > 100)
-    {
-        a = num / 100;
-        cout << "no. of 100s notes needed will be: " << a << endl;
-        num = num - a * 100;
-    }
-    if (num > 50)
-    {
-        a = num / 50;
-        cout << "no. of 50s notes needed will be: " << a << endl;
-        num = num - a * 50;
-    }
-    if (num > 20)
-    {
-        a = num / 20;
-        cout << "no. of 20s notes needed will be: " << a << endl;
+    int num_of_hundreds=0;
+    int num_of_fiftys=0;
+    int num_of_twentys=0;
+    int num_of_ones=0;
+    int num;
+    cout << "enter a number" << endl;
+    cin >> num;
 
-        num = num - a * 20;
-    }
-    if (num > 1)
+    switch (num > 100)
     {
-        a = num / 1;
-        cout << "no. of 1s notes needed will be: " << a << endl;
+    case true:
+    {
+        num_of_hundreds = num / 100;
 
-        num = num - a * 1;
+        num = num - num_of_hundreds * 100;
     }
+    }
+    switch (num > 50)
+    {
+    case true:
+    {
+        num_of_fiftys = num / 50;
+        num = num - num_of_fiftys * 50;
+    }
+    }
+    switch (num > 20)
+    {
+    case true:
+    {
+        num_of_twentys = num / 20;
+
+        num = num - num_of_twentys * 20;
+    }
+    }
+    switch (num > 1)
+    {
+    case true:
+    {
+        num_of_ones = num / 1;
+
+        num = num - num_of_ones * 1;
+    }
+    }
+    cout << "no. of 100s notes needed will be: " << num_of_hundreds << endl;
+    cout << "no. of 50s notes needed will be: " << num_of_fiftys << endl;
+    cout << "no. of 20s notes needed will be: " << num_of_twentys << endl;
+    cout << "no. of 1s notes needed will be: " << num_of_ones << endl;
 }

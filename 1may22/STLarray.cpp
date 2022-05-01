d50d0213e88d370e4ca3cfcc325426e740b524fa
ananya karne <ananyakarne02@gmail.com>
2022-05-01 18:24:19 +0530

    STL printing array, can be accessed with [ ]

--- 1may22/STLarray.cpp
+++ 1may22/STLarray.cpp
@@ -2,8 +2,12 @@
 #include <array>
 using namespace std;
 int main(){
-    int ar[]={1,2,3,4};
+    // int ar[]={1,2,3,4};
     array <int ,4> a={2,3,4,5};
     int size=a.size();//only for STL array, thats why used 'a' and not 'ar'
-    cout<<size;
+    cout<<size<<endl;
+    cout<<"the array is :"<<endl;
+    for(int i=0;i<size;i++){
+        cout<<a[i]<<" ";   
+    }
 }

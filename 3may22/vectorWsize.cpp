3988af54c57b4b237a0bb3735343ff33ca40f231
ananya karne <ananyakarne02@gmail.com>
2022-05-03 21:11:56 +0530

    vector2

--- 3may22/vectorWsize.cpp
+++ 3may22/vectorWsize.cpp
@@ -4,8 +4,16 @@ using namespace std;
 int main(){
     vector<int> a(5,2);
     cout<< "the a vector is :"<<endl;
-    // use loop for printing
+    // use loop for printings
     for(int i:a){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    // for copying
+    vector<int> b(a);
+    cout<< "the b vector is :"<<endl;
+
+      for(int i:b){
         cout<<i<<" ";
     }
 }

325dd600dc9d6c424c903b0ad8868731990a6d52
ananya karne <ananyakarne02@gmail.com>
2022-05-03 20:46:10 +0530

    --vector--

--- 3may22/VECTOR1.cpp
+++ 3may22/VECTOR1.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include <vector> //include the library!
 using namespace std;
-int main(){
-    vector<int> v; //initiallising a vector, when avector is made, its size as well as memory alloted to it is zero.
-    cout<<"size: "<<v.capacity()<<endl;
+int main()
+{
+
+    vector<int> v; // initiallising a vector, when avector is made, its size as well as memory alloted to it is zero.
+    cout << "size: " << v.capacity() << endl;
+
+    // for adding elements
+    v.push_back(1);
+    cout << "size: " << v.capacity() << endl;
+
+    v.push_back(2);
+    cout << "size: " << v.capacity() << endl;
+
+    v.push_back(3);
+    cout << "size: " << v.capacity() << endl;
+
+    v.push_back(4);
+    cout << "size: " << v.capacity() << endl;
+
+    v.push_back(4);
+    cout << "size: " << v.capacity() << endl;
+
+    v.push_back(4);
+    cout << "size: " << v.capacity() << endl;
+
+    v.push_back(4);
+    cout << "size: " << v.capacity() << endl;
+    // so the vector doubles itself as soon it gets filled
 }

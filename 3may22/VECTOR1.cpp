81a554202decdc83d76bdbcefa67477eb35ced60
ananya karne <ananyakarne02@gmail.com>
2022-05-03 20:38:52 +0530

    --vector--

---
+++ 3may22/VECTOR1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector> //include the library!
+using namespace std;
+int main(){
+    vector<int> v; //initiallising a vector, when avector is made, its size as well as memory alloted to it is zero.
+    cout<<"size: "<<v.capacity()<<endl;
+}
